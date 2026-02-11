0f8ac712549f1fcaf5d217d2af75958ae42e6442
Colo482 <Ignaciobonifacio4@gmail.com>
2026-02-11 16:15:29 -0300

    Habilite el CORS para el Front en la nube

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://192.168.0.37:5173",  # <--- AGREGÁ ESTO
+    "http://192.168.0.37:5173",
+    "https://pastillapp.vercel.app/",
 ]
 
 # --- STATIC FILES (CSS, JavaScript, Images) ---
