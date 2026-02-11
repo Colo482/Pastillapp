2fe1b1c444aa25b766128783dcf21f307061716c
Colo482 <Ignaciobonifacio4@gmail.com>
2026-02-11 15:33:23 -0300

    Limpieza de wsgi (borre la cracion de superuser)

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -6,15 +6,3 @@ from django.contrib.auth import get_user_model
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 application = get_wsgi_application()
 
-# --- BLOQUE PARA CREAR TU ADMIN ---
-try:
-    User = get_user_model()
-    # Cambiá 'admin' y 'tu_password' por lo que quieras usar
-    if not User.objects.filter(username='Colo').exists():
-        User.objects.create_superuser('Colo', 'ignaciobonifacio4@gmail.com', 'Fausto2021')
-        print("Superusuario 'Colo' creado con éxito.")
-    else:
-        print("El usuario ya existe, no se creó de nuevo.")
-except Exception as e:
-    print(f"Error creando superusuario: {e}")
-# ----------------------------------
